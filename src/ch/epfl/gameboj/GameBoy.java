6674007b4d7fcd3b795fbb90ea370223e3c1494d
Sylvain <sylvaink@bluewin.ch>
2018-02-23 16:19:27 +0100

    add comments to Gameboy

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -3,12 +3,21 @@ package ch.epfl.gameboj;
 import ch.epfl.gameboj.component.memory.Ram;
 import ch.epfl.gameboj.component.memory.RamController;
 
+/**
+ * Represents a gameboy
+ * @author luca
+ *
+ */
 public class GameBoy {
     
     private Bus mBus;
     private RamController workRam;
     private RamController echoRam;
     
+    /**
+     * Creates a new gameboy, with a cartrige inserted
+     * @param cartrige :
+     */
     public GameBoy(Object cartrige) {
         Ram ram = new Ram(AddressMap.WORK_RAM_SIZE);
         workRam = new RamController(ram, AddressMap.WORK_RAM_START, AddressMap.WORK_RAM_END);
@@ -19,6 +28,9 @@ public class GameBoy {
         mBus.attach(echoRam);
     }
     
+    /**
+     * @return the main bus of the gameboy
+     */
     public Bus bus() {
         return mBus;
     }
