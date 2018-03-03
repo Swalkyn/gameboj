bd116fd35f89a1807a1232d7ed026c924f5591fd
Sylvain <sylvaink@bluewin.ch>
2018-03-03 23:48:47 +0100

    add javadoc to Cpu

--- src/ch/epfl/gameboj/component/cpu/Cpu.java
+++ src/ch/epfl/gameboj/component/cpu/Cpu.java
@@ -7,7 +7,6 @@ import ch.epfl.gameboj.Bus;
 import ch.epfl.gameboj.Preconditions;
 import ch.epfl.gameboj.Register;
 import ch.epfl.gameboj.RegisterFile;
-import ch.epfl.gameboj.bits.Bit;
 import ch.epfl.gameboj.bits.Bits;
 import ch.epfl.gameboj.component.Clocked;
 import ch.epfl.gameboj.component.Component;
@@ -43,144 +42,14 @@ public final class Cpu implements Component, Clocked {
     
     private RegisterFile<Reg> rf = new RegisterFile<>(Reg.values());
     
+    
+ /* Methods for Component interface */
+    
     @Override
     public void attachTo(Bus bus) {
         this.bus = bus;
         Component.super.attachTo(bus);
     }
-    
-    
-    /* Read and write */
-    
-    private int read8(int address) {
-        Preconditions.checkBits16(address);
-        
-        return bus.read(address);
-    }
-    
-    private int read8AtHl() {
-        return read8(reg16(Reg16.HL));
-    }
-    
-    private int read8AfterOpcode() {
-        return read8(PC + 1);
-    }
-    
-    private int read16(int address) {
-        Preconditions.checkBits16(address);
-        
-        int lsb = bus.read(address);
-        int msb = bus.read(address + 1);
-        
-        return Bits.make16(msb, lsb);
-    }
-    
-    private int read16AfterOpcode() {
-        return read16(PC + 1);
-    }
-    
-    private void write8(int address, int v) {
-        Preconditions.checkBits16(address);
-        Preconditions.checkBits8(v);
-        
-        bus.write(address, v);
-    }
-    
-    private void write16(int address, int v) {
-        Preconditions.checkBits16(address);
-        Preconditions.checkBits16(v);
-        
-        int lsb = Bits.clip(8, v);
-        int msb = Bits.extract(v, 8, 8);
-        
-        bus.write(address, lsb);
-        bus.write(address + 1, msb);
-    }
-    
-    private void write8AtHl(int v) {        
-        write8(reg16(Reg16.HL), v);
-    }
-    
-    private void push16(int v) {
-        SP -= 2;
-        write16(SP, v);
-    }
-    
-    private int pop16() {
-        int value = read16(SP);
-        SP += 2;
-        return value;
-    }
-    
-    
-    /* Getters and setters for 16-bit registers */
-    
-    private int reg16(Reg16 r) {
-        int lsb = rf.get(r.r1);
-        int msb = rf.get(r.r2);
-        
-        return Bits.make16(msb, lsb);
-    }
-    
-    private void setReg16(Reg16 r, int newV) {
-        Preconditions.checkBits16(newV);
-        
-        int lsb = Bits.clip(newV, 8);
-        int msb = Bits.extract(newV, 8, 8);
-        
-        if (r == Reg16.AF) {
-            lsb = 0;
-        }
-        
-        rf.set(r.r1, lsb);
-        rf.set(r.r2, msb);
-    }
-    
-    private void setReg16SP(Reg16 r, int newV) {
-        if (r == Reg16.AF) {
-            SP = newV;
-        } else {
-            setReg16(r, newV);
-        }
-    }
-    
-
-    /* Bit extraction */
-    
-    private Reg extractReg(Opcode opcode, int startBit) {
-        int registerCode = Bits.extract(opcode.encoding, startBit, 3);
-        
-        switch(registerCode) {
-            case 0b000: return Reg.B;
-            case 0b001: return Reg.C;
-            case 0b010: return Reg.D;
-            case 0b011: return Reg.E;
-            case 0b100: return Reg.H;
-            case 0b101: return Reg.L;
-            case 0b111: return Reg.A;
-            
-            default : throw new IllegalArgumentException("Invalid register code");
-        }
-    }
-    
-    private Reg16 extractReg16(Opcode opcode) {
-        int registerCode = Bits.extract(opcode.encoding, 4, 2);
-        
-        // TODO : ask for AF/SP conflict
-        switch(registerCode) {
-            case 0b00: return Reg16.BC;
-            case 0b01: return Reg16.DE;
-            case 0b10: return Reg16.HL;
-            case 0b11: return Reg16.AF;
-
-            default : throw new IllegalArgumentException("Invalid register code");
-        }
-    }
-    
-    private int extractHlIncrement(Opcode opcode) {
-        return Bits.test(opcode.encoding, 4) ? -1 : 1;
-    }
-    
     
     @Override
     public int read(int address) {
@@ -197,10 +66,227 @@ public final class Cpu implements Component, Clocked {
         
         // If processor has something to do
         if (cycle == nextNonIdleCycle) {
-            
+            dispatch(read8(PC));
+        } 
+        /*else {
+            dispatch(Opcode.NOP.encoding);
+        }*/
+    }
+    
+    
+    /* Read and write */
+    
+    /**
+     * Reads an 8-bit value on the bus at specified address
+     * @param address : the address to be read
+     * @return the read value
+     */
+    private int read8(int address) {
+        Preconditions.checkBits16(address);
+        
+        return bus.read(address);
+    }
+    
+    /**
+     * Reads an 8-bit value on the bus at the address stored in the HL register
+     * @return the read value
+     */
+    private int read8AtHl() {
+        return read8(reg16(Reg16.HL));
+    }
+    
+    /**
+     * Reads an 8-bit value at the address following the opcode's address 
+     * @return the read value
+     */
+    private int read8AfterOpcode() {
+        return read8(PC + 1);
+    }
+    
+    /**
+     * Reads a 16-bit value on the bus at specified address
+     * @param address : the address to be read
+     * @return the read value
+     */
+    private int read16(int address) {
+        Preconditions.checkBits16(address);
+        
+        int lsb = bus.read(address);
+        int msb = bus.read(address + 1);
+        
+        return Bits.make16(msb, lsb);
+    }
+    
+    /**
+     * Reads a 16-bit value at the address following the opcode's address
+     * @return the read value
+     */
+    private int read16AfterOpcode() {
+        return read16(PC + 1);
+    }
+    
+    /**
+     * Writes the given 8-bit value on the bus at the specified address
+     * @param address : the address were the value will be written
+     * @param v : the value to be written
+     */
+    private void write8(int address, int v) {
+        Preconditions.checkBits16(address);
+        Preconditions.checkBits8(v);
+        
+        bus.write(address, v);
+    }
+    
+    /**
+     * Writes the given 16-bit value on the bus at the specified address
+     * @param address : the address were the value will be written
+     * @param v : the value to be written
+     */
+    private void write16(int address, int v) {
+        Preconditions.checkBits16(address);
+        Preconditions.checkBits16(v);
+        
+        int lsb = Bits.clip(8, v);
+        int msb = Bits.extract(v, 8, 8);
+        
+        bus.write(address, lsb);
+        bus.write(address + 1, msb);
+    }
+    
+    /**
+     * Writes the given 8-bit value on the bus at the address stored in the HL register
+     * @param v
+     */
+    private void write8AtHl(int v) {        
+        write8(reg16(Reg16.HL), v);
+    }
+    
+    /**
+     * Writes the given 16-bit value at the address stored in SP - 2
+     * @param v : the value to be written
+     */
+    private void push16(int v) {
+        SP -= 2;
+        write16(SP, v);
+    }
+    
+    /**
+     * Reads the value at the address stored in the SP register, and decrements the address by 2
+     * @return
+     */
+    private int pop16() {
+        int value = read16(SP);
+        SP += 2;
+        return value;
+    }
+    
+    
+    /* Getters and setters for 16-bit registers */
+    
+    /**
+     * Reads a value stored in a 16-bit register
+     * @param r : the register where the value is stored
+     * @return the value
+     */
+    private int reg16(Reg16 r) {
+        int lsb = rf.get(r.r1);
+        int msb = rf.get(r.r2);
+        
+        return Bits.make16(msb, lsb);
+    }
+    
+    /**
+     * Stores a given 16-bit value in the specified 16-bit register
+     * @param r : the register where the value will be stored
+     * @param newV : the value to store
+     */
+    private void setReg16(Reg16 r, int newV) {
+        Preconditions.checkBits16(newV);
+        
+        int lsb = Bits.clip(newV, 8);
+        int msb = Bits.extract(newV, 8, 8);
+        
+        if (r == Reg16.AF) {
+            lsb = 0;
+        }
+        
+        rf.set(r.r1, lsb);
+        rf.set(r.r2, msb);
+    }
+    
+    /**
+     * Stores a given 16-bit value in the specified 16-bit register, but if the the specified register is AF, stores the value in SP instead
+     * @param r : the register where the value will be stored
+     * @param newV : the value to store
+     */
+    private void setReg16SP(Reg16 r, int newV) {
+        if (r == Reg16.AF) {
+            SP = newV;
+        } else {
+            setReg16(r, newV);
         }
     }
     
+
+    /* Bit extraction */
+    
+    /**
+     * Extracts the 8-bit reg's code from an opcode at a specified index
+     * @param opcode : the opcode from which the reg's code will be extracted
+     * @param startBit : the index where the 3 bits long code starts
+     * @return the reg's code
+     */
+    private Reg extractReg(Opcode opcode, int startBit) {
+        int registerCode = Bits.extract(opcode.encoding, startBit, 3);
+        
+        switch(registerCode) {
+            case 0b000: return Reg.B;
+            case 0b001: return Reg.C;
+            case 0b010: return Reg.D;
+            case 0b011: return Reg.E;
+            case 0b100: return Reg.H;
+            case 0b101: return Reg.L;
+            case 0b111: return Reg.A;
+            
+            default : throw new IllegalArgumentException("Invalid register code");
+        }
+    }
+    
+    /**
+     * Extracts the 16-bit reg's code from an opcode
+     * @param opcode : the opcode from which the reg's code will be extracted
+     * @return the reg's code
+     */
+    private Reg16 extractReg16(Opcode opcode) {
+        int registerCode = Bits.extract(opcode.encoding, 4, 2);
+        
+        switch(registerCode) {
+            case 0b00: return Reg16.BC;
+            case 0b01: return Reg16.DE;
+            case 0b10: return Reg16.HL;
+            case 0b11: return Reg16.AF;
+
+            default : throw new IllegalArgumentException("Invalid register code");
+        }
+    }
+    
+    /**
+     * Extract whether the value in HL should be incremented or decremented from an opcode
+     * @param opcode : the opcode from which the increment will be extracted
+     * @return the increments (1 or -1)
+     */
+    private int extractHlIncrement(Opcode opcode) {
+        return Bits.test(opcode.encoding, 4) ? -1 : 1;
+    }
+    
+   
+    /* Opcode table methods */
+    
+    /**
+     * Creates a table of opcodes, selected by their kind
+     * @param kind : the kind of opcodes the table will contain
+     * @return the table of opcodes
+     */
     private static Opcode[] buildOpcodeTable(Kind kind) {
         ArrayList<Opcode> table = new ArrayList<>();
         
@@ -213,6 +299,11 @@ public final class Cpu implements Component, Clocked {
         return table.toArray(new Opcode[table.size()]);
     }
     
+    /**
+     * Searches an opcode by its encoding in an opcode table
+     * @param opcodeEncoding : the encoding of the opcode
+     * @return the opcode
+     */
     private Opcode searchOpcodeTable(int opcodeEncoding) {
         Preconditions.checkBits8(opcodeEncoding);
         
@@ -225,6 +316,10 @@ public final class Cpu implements Component, Clocked {
         throw new IllegalArgumentException("Opcode encoding does not exist");
     }
     
+    /**
+     * Executes a task corresponding to an opcode, and increments the program counter and the next non-idle cycle
+     * @param opcodeEncoding : the opcode encoding
+     */
     public void dispatch(int opcodeEncoding) {
         Preconditions.checkBits8(opcodeEncoding);
         
@@ -315,13 +410,9 @@ public final class Cpu implements Component, Clocked {
     
             default:
                 break;
-            }
+        }
         
-        // TODO nextNonIdleCycle, PC
+        PC += opcode.totalBytes;
+        nextNonIdleCycle += opcode.cycles;
     }
-    
-    
-    
-    
-    
 }
