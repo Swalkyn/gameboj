480a31e464216d2bdf83fd7e8ac405889bcfa90d
Sylvain <sylvaink@bluewin.ch>
2018-03-14 18:36:51 +0100

    ProgramBuilder finished

--- test/ch/epfl/test/ProgramBuilder.java
+++ test/ch/epfl/test/ProgramBuilder.java
@@ -2,13 +2,18 @@ package ch.epfl.test;
 
 import java.util.ArrayList;
 
+import ch.epfl.gameboj.Bus;
 import ch.epfl.gameboj.Preconditions;
 import ch.epfl.gameboj.bits.Bits;
+import ch.epfl.gameboj.component.cpu.Cpu;
 import ch.epfl.gameboj.component.cpu.Opcode;
+import ch.epfl.gameboj.component.memory.Ram;
+import ch.epfl.gameboj.component.memory.RamController;
 
 public class ProgramBuilder {
     
     private ArrayList<Integer> program;
+    private Cpu cpu;
     public static final int PREFIX = 0xCB;
     
     public ProgramBuilder() {
@@ -45,7 +50,7 @@ public class ProgramBuilder {
         program.add(value);
     }
     
-    public int[] build() {
+    private int[] build() {
         int[] prog = new int[program.size()];
         
         for (int i = 0; i < program.size(); i++) {
@@ -53,5 +58,42 @@ public class ProgramBuilder {
         }
         
         return prog;
+    }
+    
+    private Cpu newCpu(int[] program) {
+        Ram ram = new Ram(program.length);
+        RamController rc = new RamController(ram, 0);
+        Bus bus = new Bus();        
+        Cpu cpu = new Cpu();
+        
+        // Fill ram
+        for (int i = 0; i < program.length; i++) {
+            rc.write(i, program[i]);
+        }
+        
+        rc.attachTo(bus);
+        cpu.attachTo(bus);
+        
+        return cpu;
+    }
+    
+    private void runCpu(Cpu cpu, int numberOfCycles) {
+        for (long i = 0; i < numberOfCycles; i++) {
+            cpu.cycle(i);
+        }
+    }
+    
+    public void run(int cycle) {
+        int[] prog = build();
+        cpu = newCpu(prog);
+        runCpu(cpu, cycle);
+    }
+    
+    public void run() {
+        run(program.size());
+    }
+    
+    public int[] getResult() {
+        return cpu._testGetPcSpAFBCDEHL();
     }
 }
